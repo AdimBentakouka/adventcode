a90252442922cbf98d77ff7a16d8664bc8081e9a
adimb.dev <adim.bentakouka@gmail.com>
2025-01-10 19:12:09 +0100

    part2 day 1

--- 2024/day_1/index.ts
+++ 2024/day_1/index.ts
@@ -1,4 +1,4 @@
-export const main = async (input: string) : Promise<number> => {
+export const main = async (input: string) : Promise<{p1:number, p2:number}> => {
     const data = await Bun.file(input).text();
 
     const grid = data.split("\n").map(line => line.split("   "));
@@ -6,8 +6,15 @@ export const main = async (input: string) : Promise<number> => {
     const leftCol = grid.map(row => parseInt(row[0])).sort((a, b) => a - b);
     const rightCol = grid.map(row => parseInt(row[1])).sort((a, b) => a - b);
 
-    return leftCol.reduce((tmp, leftValue, i) => {
+    const p1 = leftCol.reduce((tmp, leftValue, i) => {
         const distance = Math.abs(leftValue - rightCol[i]);
         return tmp + distance;
     }, 0);
+
+    const p2 = leftCol.reduce((tmp, leftValue) => {
+        const occ = rightCol.filter((rightValue) =>  leftValue === rightValue).length;
+        return tmp + (occ * leftValue);
+    }, 0);
+
+    return {p1, p2}
 }
