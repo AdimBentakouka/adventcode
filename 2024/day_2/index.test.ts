11a76a89e465bb44009914eaadcb44b482e94c6d
adimb.dev <adim.bentakouka@gmail.com>
2025-01-11 23:57:07 +0100

    Day 2 => p2

--- 2024/day_2/index.test.ts
+++ 2024/day_2/index.test.ts
@@ -1,11 +1,31 @@
-import {expect, test} from "bun:test";
+import {expect, test, describe} from "bun:test";
 import {main} from "./index.ts";
 
-test("it should work for test data", async() => {
-    expect(await main('./input/example.txt')).toBe(2);
+describe("p1", () => {
+    test("it should work for test data", async() => {
+        expect((await main('./input/example.txt')).p1).toBe(2);
+    })
+
+
+    test("it should work for real data", async() => {
+        expect((await main('./input/source.txt')).p1).toBe(526);
+    })
+
+
+
 })
 
 
-test("it should work for real data", async() => {
-   expect(await main('./input/source.txt')).toBe(526);
+describe("p2", () => {
+    test("it should work for test data", async() => {
+        expect((await main('./input/example.txt')).p2).toBe(4);
+    })
+
+
+    test("it should work for real data", async() => {
+       expect((await main('./input/source.txt')).p2).toBe(566);
+    })
+
 })
+
+
--- 2024/day_2/index.ts
+++ 2024/day_2/index.ts
@@ -1,30 +1,55 @@
 const MAX_DISTANCE = 3;
 const MIN_DISTANCE = 1;
 
-export const main = async (input: string) : Promise<number> => {
-    const data = await Bun.file(input).text();
 
-    return data.split("\n").reduce((tmp, line) => {
-        return tmp + Number(isRowSafe(line.split(" ")));
+export const main = async (input: string): Promise<{ p1: number, p2: number }> => {
+    const data = await Bun.file(input).text();
+    const lines = data.trim().split('\n');
+
+    const p1 = lines.reduce((acc, line) => acc + Number(isRowSafe(line.split(' ').map(Number))), 0);
+    const p2 = lines.reduce((acc, line) => {
+        const row = line.split(' ').map(Number);
+         if(isRowSafe(row))
+         {
+             return acc + 1;
+         }
+
+        for(let i = 0; i < row.length; i++)
+        {
+            const newRow = [...row.slice(0, i), ...row.slice(i + 1)];
+            if (isRowSafe(newRow)) {
+                return acc + 1;
+            }
+        }
+
+        return acc;
     }, 0);
+
+
+    return {p1, p2}
 }
 
-const isRowSafe = (row : string[]) : boolean => {
 
-    const isAscending = parseInt(row[1]) > parseInt(row[0]);
+const isRowSafe = (row: number[]) => {
+
+    const rowSortedByAsc = [...row].sort((a, b) => a - b);
+    const rowSortedByDesc = [...row].sort((a, b) => b - a);
+
+    const isSorted =
+        row.every((value, index) => value === rowSortedByAsc[index]) ||
+        row.every((value, index) => value === rowSortedByDesc[index]);
 
 
-    for(let i = 1; i < row.length; i++) {
-        const previousValue = parseInt(row[i - 1]);
-        const currentValue = parseInt(row[i]);
-
-        if((isAscending && currentValue < previousValue )|| (!isAscending && previousValue < currentValue))
-            return false;
+    for (let i = 1; i < row.length; i++) {
+        const previousValue = row[i - 1];
+        const currentValue = row[i];
 
         const distance = Math.abs(currentValue - previousValue);
-        
-        if(distance > MAX_DISTANCE || distance < MIN_DISTANCE) return false;
+
+        if (distance < MIN_DISTANCE || distance > MAX_DISTANCE) {
+            return false
+        }
     }
 
-    return true;
+    return isSorted;
 }
